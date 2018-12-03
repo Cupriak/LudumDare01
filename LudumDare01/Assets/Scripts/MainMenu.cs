e56e7fc1926f0d843605fc7a0353dcc8ad0b177a
Mateusz Cupriak <mcupriak95@gmail.com>
2018-12-03 03:00:05 +0100

    Initial Main Menu

---
+++ LudumDare01/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string sceneNameToLoadAtStart = "Yanush";
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneNameToLoadAtStart);
+    }
+
+    public void CloseGame()
+    {
+        Debug.Log("Closing Game function called!");
+        Application.Quit();
+    }
+}
---
+++ LudumDare01/Assets/Scripts/MainMenuButtonOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtonOnClick : MonoBehaviour {
+
+   public void PlaySoundEffect()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
